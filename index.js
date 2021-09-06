96cba48ab6b6e6ccf93eb15cb60e37b99d3939f7
Rashedul Hasan <Rashedul Hasan>
2021-09-06 12:28:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// var num1 = 10;
+// var num2 = "15.9";
+// var num2 = parseFloat(num2);
+// console.log(num1 + num2);
+
+// var num = 234.4657657;
+// num++;
+// num--;
+// console.log(num.toFixed(2));
+
+// var num = Math.ceil(15.5214);
+// console.log(num);
+
+// var price1 = 10;
+// var price2 = 3;
+// var add = price1+price2;
+// var sub = price1-price2;
+// var muti = price1*price2;
+// var div = price1/price2;
+// var rem = price1%price2;
+// console.log("Add two number:",add);
+// console.log("Sub two number:",sub);
+// console.log("Multi two number:",muti);
+// console.log("Div two number:",div.toFixed(2));
+// console.log("Rem two number:",rem);
+
+
+// var num = 15;
+// if(num == 15){
+//     console.log(num,"is equal 15");
+// }
+// else{
+//     console.log(num,"is not not 15");
+// }
+
+var x = 5;
+var y = 5;
+if(x == 10 && y == 5){
+    console.log("Gora gori korta jaba -first");
+}
+else if(x == 10 || y==10){
+    console.log("Gora gori korta jaba");
+}
+else{
+    console.log("Gora gori korta jaba nah");
+}
+
+
+
+
+
